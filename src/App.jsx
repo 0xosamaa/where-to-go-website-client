f0d800cdb8a83f8997089cb32be40717cf727185
swe-amr-abdelaziz <swe.amr.abdelaziz@gmail.com>
2023-06-12 15:31:25 +0300

    Created main theme colors and typography

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button, Typography } from '@mui/material'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,18 +10,34 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <Typography variant="h1">Typography h1</Typography>
+      <Typography variant="h2">Typography h2</Typography>
+      <Typography variant="h3">Typography h3</Typography>
+      <Typography variant="h4">Typography h4</Typography>
+      <Typography variant="h5">Typography h5</Typography>
+      <Typography variant="h6">Typography h6</Typography>
+      <Typography variant="body">Normal Typography</Typography>
+      
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <Button color="primary" variant="contained" onClick={() => setCount((count) => count + 1)}>
+          Primary {count}          
+        </Button>
+        <Button color="secondary" variant="contained" onClick={() => setCount((count) => count + 1)}>
+          Secondary is {count}
+        </Button>
+        <Button color="primary" variant="outlined" onClick={() => setCount((count) => count + 1)}>
+          Primary {count}          
+        </Button>
+        <Button color="secondary" variant="outlined" onClick={() => setCount((count) => count + 1)}>
+          Secondary is {count}
+        </Button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
