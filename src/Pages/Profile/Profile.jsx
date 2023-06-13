b6a76279d079ac5757dfd9d7a74296acd42688e5
Hassan_Osama <hassan.osama720@gmail.com>
2023-06-13 14:18:14 +0300

    init

---
+++ src/Pages/Profile/Profile.jsx
@@ -0,0 +1,13 @@
+
+
+const Profile = () => {
+    return (
+        <div className="container d-flex flex-sm-column flex-lg-row justify-content-lg-between align-items-lg-center justify-content-sm-center align-items-sm-center">
+            <div className="sidebar">
+                <typography variant="h1" className="text-center">Profile</typography>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './Layout/Layout';
 import Home from './Pages/Home/Home';
+import Profile from "./Pages/Profile/Profile.jsx";
 import React from 'react';
 
 const AppRoutes = () => {
@@ -9,6 +10,10 @@ const AppRoutes = () => {
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
+                    <Route />
+                </Route>
+                <Route path="/profile" element={<Layout />}>
+                    <Route index element={<Profile />} />
                     <Route />
                 </Route>
             </Routes>
