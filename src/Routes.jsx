6db55975e4d93dc2e0b7a2a303b42e2f10dc09cc
swe-amr-abdelaziz <swe.amr.abdelaziz@gmail.com>
2023-06-13 14:45:04 +0300

    Added SearchResults & VendorDetails components

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './Layout/Layout';
 import Home from './Pages/Home/Home';
+import SearchResults from './Pages/SearchResults/SearchResults'
 import React from 'react';
+import VendorDetails from './Pages/VendorDetails/VendorDetails';
 
 const AppRoutes = () => {
     return (
@@ -9,7 +11,8 @@ const AppRoutes = () => {
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
-                    <Route />
+                    <Route path="/search" element={<SearchResults />} />
+                    <Route path="/vendor" element={<VendorDetails />} />
                 </Route>
             </Routes>
         </Router>
