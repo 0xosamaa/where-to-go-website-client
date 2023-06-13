aa541615ac1145bf39d1b4ca1b4479256a0c9ebf
Osama <0xm.osamaa@gmail.com>
2023-06-13 14:00:13 +0300

    moved theme config to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,74 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button, Typography } from '@mui/material'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <Typography variant="h1">Typography h1</Typography>
-      <Typography variant="h2">Typography h2</Typography>
-      <Typography variant="h3">Typography h3</Typography>
-      <Typography variant="h4">Typography h4</Typography>
-      <Typography variant="h5">Typography h5</Typography>
-      <Typography variant="h6">Typography h6</Typography>
-      <Typography variant="body">Normal Typography</Typography>
-      
-      <div className="card">
-      <Button color="primary" variant="contained" onClick={() => setCount((count) => count + 1)}>
-          Primary {count}          
-        </Button>
-        <Button color="secondary" variant="contained" onClick={() => setCount((count) => count + 1)}>
-          Secondary is {count}
-        </Button>
-        <Button color="primary" variant="outlined" onClick={() => setCount((count) => count + 1)}>
-          Primary {count}          
-        </Button>
-        <Button color="secondary" variant="outlined" onClick={() => setCount((count) => count + 1)}>
-          Secondary is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-=======
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import AppRoutes from './Routes';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#00BBAA',
+            light: '#07ffea',
+            dark: '#00a99b',
+            contrastText: '#FFFFFF',
+        },
+        secondary: {
+            main: '#0053CC',
+            light: '#1673ff',
+            dark: '#0043a8',
+            contrastText: '#FFFFFF',
+        },
+    },
+    typography: {
+        fontFamily: 'Roboto, sans-serif',
+        fontSize: 16,
+        fontWeightLight: 300,
+        fontWeightRegular: 400,
+        fontWeightMedium: 500,
+        fontWeightBold: 700,
+        h1: {
+            fontFamily: 'Archivo, sans-serif',
+            fontWeight: 700,
+            fontSize: 40,
+        },
+        h2: {
+            fontFamily: 'Archivo, sans-serif',
+            fontWeight: 700,
+            fontSize: 32,
+        },
+        h3: {
+            fontFamily: 'Archivo, sans-serif',
+            fontWeight: 700,
+            fontSize: 24,
+        },
+        h4: {
+            fontFamily: 'Archivo, sans-serif',
+            fontWeight: 700,
+            fontSize: 20,
+        },
+        h5: {
+            fontFamily: 'Archivo, sans-serif',
+            fontWeight: 700,
+            fontSize: 18,
+        },
+        h6: {
+            fontFamily: 'Archivo, sans-serif',
+            fontWeight: 700,
+            fontSize: 16,
+        },
+        body: {
+            fontFamily: 'Roboto, sans-serif',
+            fontWeight: 400,
+            fontSize: 16,
+        },
+    },
+});
 
 function App() {
-    return <AppRoutes />;
->>>>>>> dev
+    return (
+        <ThemeProvider theme={theme}>
+            <AppRoutes />
+        </ThemeProvider>
+    );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,73 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createTheme, ThemeProvider } from '@mui/material'
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#00BBAA',
-      light: '#07ffea',
-      dark: '#00a99b',
-      contrastText: '#FFFFFF',
-    },
-    secondary: {
-      main: '#0053CC',
-      light: '#1673ff',
-      dark: '#0043a8',
-      contrastText: '#FFFFFF',
-    }
-  },
-  typography: {
-    fontFamily: 'Roboto, sans-serif',
-    fontSize: 16,
-    fontWeightLight: 300,
-    fontWeightRegular: 400,
-    fontWeightMedium: 500,
-    fontWeightBold: 700,
-    h1: {
-      fontFamily: 'Archivo, sans-serif',
-      fontWeight: 700,
-      fontSize: 40,
-    },
-    h2: {
-      fontFamily: 'Archivo, sans-serif',
-      fontWeight: 700,
-      fontSize: 32,
-    },
-    h3: {
-      fontFamily: 'Archivo, sans-serif',
-      fontWeight: 700,
-      fontSize: 24,
-    },
-    h4: {
-      fontFamily: 'Archivo, sans-serif',
-      fontWeight: 700,
-      fontSize: 20,
-    },
-    h5: {
-      fontFamily: 'Archivo, sans-serif',
-      fontWeight: 700,
-      fontSize: 18,
-    },
-    h6: {
-      fontFamily: 'Archivo, sans-serif',
-      fontWeight: 700,
-      fontSize: 16,
-    },
-    body: {
-      fontFamily: 'Roboto, sans-serif',
-      fontWeight: 400,
-      fontSize: 16,
-    }
-  }
-})
-
-  
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ThemeProvider theme={theme}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  </ThemeProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
