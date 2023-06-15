0c1394505ab26a3c895d1be2eb5ea5f961a0c4d3
Osama <0xm.osamaa@gmail.com>
2023-06-15 18:25:40 +0300

    wip: login and register

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './Layout/Layout';
 import Home from './Pages/Home/Home';
-import Profile from "./Pages/Profile/Profile.jsx";
-import SearchResults from './Pages/SearchResults/SearchResults'
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
+import Profile from './Pages/Profile/Profile.jsx';
+import SearchResults from './Pages/SearchResults/SearchResults';
 import React from 'react';
 import PlaceDetails from './Pages/PlaceDetails/PlaceDetails';
-import ContactUs from "./Pages/ContactUs/ContactUs.jsx";
-import Page404 from "./Pages/404/404.jsx";
+import ContactUs from './Pages/ContactUs/ContactUs.jsx';
+import Page404 from './Pages/404/404.jsx';
 
 const AppRoutes = () => {
     return (
         <Router>
             <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
                     <Route path="/search" element={<SearchResults />} />
