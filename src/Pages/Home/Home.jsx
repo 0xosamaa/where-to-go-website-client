c06ea91a2d47bcc301fde05f14aa0292c9117ab5
0xosamaa <0xm.osamaa@gmail.com>
2023-06-26 00:03:24 +0300

    near by places in homepage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,10 @@
-import React, {useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
 import { Link, useNavigate } from 'react-router-dom';
 import { Typography, Container, useTheme } from '@mui/material';
-import Slider from '../../Components/Slider/Slider';
+import axios from 'axios';
+// import Slider from '../../Components/Slider/Slider';
+import { useDispatch, useSelector } from 'react-redux';
 import Header from '../../Components/Header/Header';
 import top1 from '../../assets/images/places/top-rated/top-rated-1.jpeg';
 import top2 from '../../assets/images/places/top-rated/top-rated-2.jpeg';
@@ -12,22 +14,47 @@ import hotels from '../../assets/images/places/categories/hotels.jpeg';
 import restaurants from '../../assets/images/places/categories/restaurants.jpeg';
 import openPlaces from '../../assets/images/places/categories/open-places.jpeg';
 import parks from '../../assets/images/places/categories/parks.jpeg';
-import axios from "axios";
 
 const Home = () => {
     const theme = useTheme();
     const [topRated, setTopRated] = useState([]);
+    const [nearby, setNearby] = useState([]);
     const navigate = useNavigate();
+    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
     useEffect(() => {
-        axios.get("http://localhost:8001/api/v1/auth/topRated")
+        axios
+            .get('http://localhost:8001/api/v1/auth/topRated')
             .then((res) => {
                 setTopRated(res.data.data);
             })
             .catch((err) => {
-                navigate('/500')
-            })
-    }, [])
+                navigate('/500');
+            });
+    }, []);
+
+    useEffect(() => {
+        if (!isLoggedIn) return;
+
+        const token = localStorage.getItem('token');
+        (async () => {
+            const res = await axios.get(
+                'http://localhost:8001/api/v1/customers/getMe',
+                {
+                    headers: { Authorization: `Bearer ${token}` },
+                }
+            );
+
+            const user = res.data.data;
+
+            setNearby(
+                topRated.filter((place) => {
+                    return place.address.city.toLowerCase() === user.address.city.toLowerCase();
+                })
+            );
+
+        })();
+    }, [isLoggedIn, topRated]);
 
     return (
         <>
@@ -35,7 +62,10 @@ const Home = () => {
             <Container maxWidth="lg" className="top-rated my-5">
                 <div className="row">
                     <div className="col-12 my-3 text-center text-lg-start">
-                        <Typography variant="h3" color={theme.palette.primary.main}>
+                        <Typography
+                            variant="h3"
+                            color={theme.palette.primary.main}
+                        >
                             Top Rated
                         </Typography>
                     </div>
@@ -45,12 +75,24 @@ const Home = () => {
                         <Link to={`/place/${topRated[0]?._id}`}>
                             <div className="row">
                                 <div className="col-12">
-                                    <img src={topRated[0]?.thumbnail ? `http://localhost:8001/api/v1/images/vendors/${topRated[0].thumbnail}` : top1} alt="" className="mb-2" />
+                                    <img
+                                        src={
+                                            topRated[0]?.thumbnail
+                                                ? `http://localhost:8001/api/v1/images/vendors/${topRated[0].thumbnail}`
+                                                : top1
+                                        }
+                                        alt=""
+                                        className="mb-2"
+                                    />
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="col-12 text-start">
-                                    <Typography color="primary" variant="p" fontWeight={'bold'}>
+                                    <Typography
+                                        color="primary"
+                                        variant="p"
+                                        fontWeight={'bold'}
+                                    >
                                         {topRated[0]?.placeName}
                                     </Typography>
                                 </div>
@@ -61,12 +103,24 @@ const Home = () => {
                         <Link to={`/place/${topRated[1]?._id}`}>
                             <div className="row">
                                 <div className="col-12">
-                                    <img src={topRated[1]?.thumbnail ? `http://localhost:8001/api/v1/images/vendors/${topRated[1].thumbnail}` : top2} alt="" className="mb-2" />
+                                    <img
+                                        src={
+                                            topRated[1]?.thumbnail
+                                                ? `http://localhost:8001/api/v1/images/vendors/${topRated[1].thumbnail}`
+                                                : top2
+                                        }
+                                        alt=""
+                                        className="mb-2"
+                                    />
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="col-12 text-start">
-                                    <Typography color="primary" variant="p" fontWeight={'bold'}>
+                                    <Typography
+                                        color="primary"
+                                        variant="p"
+                                        fontWeight={'bold'}
+                                    >
                                         {topRated[1]?.placeName}
                                     </Typography>
                                 </div>
@@ -77,12 +131,24 @@ const Home = () => {
                         <Link to={`/place/${topRated[2]?._id}`}>
                             <div className="row">
                                 <div className="col-12">
-                                    <img src={topRated[2]?.thumbnail ? `http://localhost:8001/api/v1/images/vendors/${topRated[2].thumbnail}` : top3} alt="" className="mb-2" />
+                                    <img
+                                        src={
+                                            topRated[2]?.thumbnail
+                                                ? `http://localhost:8001/api/v1/images/vendors/${topRated[2].thumbnail}`
+                                                : top3
+                                        }
+                                        alt=""
+                                        className="mb-2"
+                                    />
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="col-12 text-start">
-                                    <Typography color="primary" variant="p" fontWeight={'bold'}>
+                                    <Typography
+                                        color="primary"
+                                        variant="p"
+                                        fontWeight={'bold'}
+                                    >
                                         {topRated[2]?.placeName}
                                     </Typography>
                                 </div>
@@ -93,12 +159,24 @@ const Home = () => {
                         <Link to={`/place/${topRated[3]?._id}`}>
                             <div className="row">
                                 <div className="col-12">
-                                    <img src={topRated[3]?.thumbnail ? `http://localhost:8001/api/v1/images/vendors/${topRated[3].thumbnail}` : top4} alt="" className="mb-2" />
+                                    <img
+                                        src={
+                                            topRated[3]?.thumbnail
+                                                ? `http://localhost:8001/api/v1/images/vendors/${topRated[3].thumbnail}`
+                                                : top4
+                                        }
+                                        alt=""
+                                        className="mb-2"
+                                    />
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="col-12">
-                                    <Typography color="primary" variant="p" fontWeight={'bold'}>
+                                    <Typography
+                                        color="primary"
+                                        variant="p"
+                                        fontWeight={'bold'}
+                                    >
                                         {topRated[3]?.placeName}
                                     </Typography>
                                 </div>
@@ -108,6 +186,58 @@ const Home = () => {
                 </div>
             </Container>
             {/* <Slider /> */}
+
+            {isLoggedIn && nearby.length !== 0 && (
+                <Container maxWidth="lg" className="nearby mb-5">
+                    <div className="row">
+                        <div className="col-12 my-3 text-center text-lg-start">
+                            <Typography
+                                variant="h3"
+                                color={theme.palette.primary.main}
+                            >
+                                Places near you
+                            </Typography>
+                        </div>
+                    </div>
+                    <div className="row">
+                        {nearby.map((place) => {
+                            return (
+                                <div className="col mb-5">
+                                    <Link
+                                        to={`/place/${place?._id}`}
+                                        key={place._id}
+                                    >
+                                        <div className="row">
+                                            <div className="col-12">
+                                                <img
+                                                    src={
+                                                        place?.thumbnail
+                                                            ? `http://localhost:8001/api/v1/images/vendors/${place.thumbnail}`
+                                                            : top1
+                                                    }
+                                                    alt=""
+                                                    className="mb-2"
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="col-12 text-start">
+                                                <Typography
+                                                    color="primary"
+                                                    variant="p"
+                                                    fontWeight={'bold'}
+                                                >
+                                                    {place?.placeName}
+                                                </Typography>
+                                            </div>
+                                        </div>
+                                    </Link>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </Container>
+            )}
             <Container maxWidth="lg" className="categories mb-5">
                 <div className="row">
                     <div className="col-12 text-center text-lg-start my-3">
