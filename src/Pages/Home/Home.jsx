72deb143630d5b9c61193dfc2bca3c4fd977e444
swe-amr-abdelaziz <swe.amr.abdelaziz@gmail.com>
2023-06-25 19:55:15 +0300

    Updated topRated client

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect,useState} from 'react';
 import './Home.css';
 import { Link, useNavigate } from 'react-router-dom';
-import { Typography, Container, useTheme } from '@mui/material';
+import { Typography, Container, useTheme, Rating, styled } from '@mui/material';
 import Slider from '../../Components/Slider/Slider';
 import Header from '../../Components/Header/Header';
 import top1 from '../../assets/images/places/top-rated/top-rated-1.jpeg';
@@ -12,17 +12,31 @@ import hotels from '../../assets/images/places/categories/hotels.jpeg';
 import restaurants from '../../assets/images/places/categories/restaurants.jpeg';
 import openPlaces from '../../assets/images/places/categories/open-places.jpeg';
 import parks from '../../assets/images/places/categories/parks.jpeg';
+import FavouriteIcon from "../../Components/FavouriteIcon/FavouriteIcon";
 import axios from "axios";
+import { useSelector } from 'react-redux';
 
 const Home = () => {
     const theme = useTheme();
+    const favoriteVendors = useSelector((state) => state.profile.favoriteVendors);
+    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
     const [topRated, setTopRated] = useState([]);
     const navigate = useNavigate();
+    
+    const StyledRating = styled(Rating)({
+        '& .MuiRating-iconFilled': {
+            color: theme.palette.primary.main,
+        },
+        '& .MuiRating-iconHover': {
+            color: '#ff3d47',
+        },
+    });
 
     useEffect(() => {
         axios.get("http://localhost:8001/api/v1/auth/topRated")
             .then((res) => {
                 setTopRated(res.data.data);
+                console.log(res.data.data);
             })
             .catch((err) => {
                 navigate('/500')
@@ -41,69 +55,39 @@ const Home = () => {
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-12 col-lg-3 mb-5">
-                        <Link to={`/place/${topRated[0]?._id}`}>
-                            <div className="row">
-                                <div className="col-12">
-                                    <img src={topRated[0]?.thumbnail ? `http://localhost:8001/api/v1/images/vendors/${topRated[0].thumbnail}` : top1} alt="" className="mb-2" />
+                    <div className="col-12 col-md-6 col-lg-3">
+                    {topRated.map((place) => {
+                        return (
+                            <>
+                                <Link to={`/place/${place?._id}`}>
+                                    <img src={place?.thumbnail ? `http://localhost:8001/api/v1/images/vendors/${place.thumbnail}` : top1} alt="" className="mb-2" />
+                                    <div className="px-2 d-flex flex-column align-items-start">
+                                        <Typography color="primary" variant="h3" fontWeight={'bold'} style={{ paddingBottom: 0 }}>
+                                            {place?.placeName}
+                                        </Typography>
+                                        <Typography color="GrayText" variant="h5" fontWeight={'bold'}>
+                                            {place?.category[0].name}
+                                        </Typography>
+                                    </div>
+                                </Link>
+                                <div className="d-flex justify-content-between align-items-center p-2">
+                                    <div className="d-flex">
+                                        <StyledRating
+                                        size="small"
+                                        name="half-rating"
+                                        defaultValue={place?.avgRate}
+                                        precision={0.5}
+                                        readOnly
+                                        />
+                                        <span className="ms-2">{place?.avgRate?.toFixed(1)}</span>
+                                    </div>
+                                    {isLoggedIn && (
+                                        <FavouriteIcon vendorId={place?._id} checked={favoriteVendors.filter((vendor) => vendor._id === place?._id).length > 0} />
+                                    )}
                                 </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-12 text-start">
-                                    <Typography color="primary" variant="p" fontWeight={'bold'}>
-                                        {topRated[0]?.placeName}
-                                    </Typography>
-                                </div>
-                            </div>
-                        </Link>
-                    </div>
-                    <div className="col-12 col-lg-3 mb-5">
-                        <Link to={`/place/${topRated[1]?._id}`}>
-                            <div className="row">
-                                <div className="col-12">
-                                    <img src={topRated[1]?.thumbnail ? `http://localhost:8001/api/v1/images/vendors/${topRated[1].thumbnail}` : top2} alt="" className="mb-2" />
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-12 text-start">
-                                    <Typography color="primary" variant="p" fontWeight={'bold'}>
-                                        {topRated[1]?.placeName}
-                                    </Typography>
-                                </div>
-                            </div>
-                        </Link>
-                    </div>
-                    <div className="col-12 col-lg-3 mb-5">
-                        <Link to={`/place/${topRated[2]?._id}`}>
-                            <div className="row">
-                                <div className="col-12">
-                                    <img src={topRated[2]?.thumbnail ? `http://localhost:8001/api/v1/images/vendors/${topRated[2].thumbnail}` : top3} alt="" className="mb-2" />
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-12 text-start">
-                                    <Typography color="primary" variant="p" fontWeight={'bold'}>
-                                        {topRated[2]?.placeName}
-                                    </Typography>
-                                </div>
-                            </div>
-                        </Link>
-                    </div>
-                    <div className="col-12 col-lg-3 mb-5">
-                        <Link to={`/place/${topRated[3]?._id}`}>
-                            <div className="row">
-                                <div className="col-12">
-                                    <img src={topRated[3]?.thumbnail ? `http://localhost:8001/api/v1/images/vendors/${topRated[3].thumbnail}` : top4} alt="" className="mb-2" />
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-12">
-                                    <Typography color="primary" variant="p" fontWeight={'bold'}>
-                                        {topRated[3]?.placeName}
-                                    </Typography>
-                                </div>
-                            </div>
-                        </Link>
+                            </>
+                        )
+                    })}
                     </div>
                 </div>
             </Container>
